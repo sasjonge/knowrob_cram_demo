43020167c7e2b946a592946eb723831e2eaf6d22
Sascha Jongebloed <sasjonge@uni-bremen.de>
2025-06-04 16:25:40 +0200

    fix to entrypoint

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
 
+echo "Starting entrypoint script..."
 source ${ROS_WS}/devel/setup.bash
-roslaunch knowrob_cram_demo knowrob.launch
+roslaunch --wait knowrob_cram_demo knowrob.launch &
+sleep 5
+roslaunch --wait rvizweb rvizweb.launch config_file:=${ROS_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
+cp ${ROS_WS}/src/knowrob_cram_demo/binder/webapps.json ${ROS_WS}/src/rvizweb/webapps/app.json
+
+xvfb-run exec "$@"
