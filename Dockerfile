aeddb58d8363b03d326e390ddb7d02a4b680ddea
Sascha Jongebloed <sasjonge@uni-bremen.de>
2025-06-04 12:23:32 +0200

    starting adding dfl reasoner

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,42 @@
-ARG FROM_IMAGE=ros:noetic-ros-core
+FROM ros:noetic-ros-core
 ARG OVERLAY_WS=/opt/ros/overlay_ws
+ARG HOME=/root
 
-FROM $FROM_IMAGE as cacher
-ARG OVERLAY_WS
-WORKDIR $OVERLAY_WS/src
+# This steps seem to be necessary for now, TODO: check if this is still needed
+# ─── STEP A: Remove any pre-existing ROS list files ─────────────────────────────────
+RUN rm -f /etc/apt/sources.list.d/*ros*.list
+# ─── STEP B: Update Ubuntu repos, install curl and gnupg2, and fetch the new ROS key ──
+RUN apt-get update && \
+    apt-get install -y curl gnupg2 lsb-release && \
+    mkdir -p /usr/share/keyrings && \
+    curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
+      -o /usr/share/keyrings/ros-archive-keyring.gpg
+# ─── STEP C: Recreate ros-latest.list so it points at our new keyring, then update ───
+RUN echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
+      http://packages.ros.org/ros/ubuntu focal main" \
+      > /etc/apt/sources.list.d/ros-latest.list && \
+    apt-get update
+# ─── STEP D: Now that APT trusts packages.ros.org, install python3-pip, vcstool, etc. ──
+RUN apt-get install -y \
+      python3-pip \
+      python3-vcstool \
+      git \
+      default-jre \
+      python3-catkin-tools && \
+    pip3 install --upgrade pip
+
 
 RUN apt-get update && apt-get install python3-pip python3-vcstool git default-jre python3-catkin-tools -y && pip3 install pip --upgrade
 RUN pip3 install rosdep && rosdep init
 
+RUN mkdir -p $OVERLAY_WS/src
 RUN vcs import --input https://raw.githubusercontent.com/Tigul/pycram-1/dev/rosinstall/pycram-https.rosinstall --skip-existing $OVERLAY_WS/src
 RUN rosdep update && rosdep install --from-paths $OVERLAY_WS/src --ignore-src -r -y
 
 RUN pip3 install --upgrade pip
 WORKDIR $OVERLAY_WS/src/pycram
-RUN pip3 install pycram_bullet==3.2.8 pathlib~=1.0.1 numpy==1.24.4 pytransform3d psutil==5.9.7 typing_extensions>=4.10.0 owlready2>=0.45 pynput~=1.7.7 dm_control \
+RUN pip3 install pycram_bullet==3.2.8 pathlib~=1.0.1 numpy==1.24.4 pytransform3d psutil==5.9.7 typing_extensions>=4.10.0 owlready2==0.47 pynput~=1.7.7 dm_control \
                 trimesh==4.6.0 deprecated probabilistic_model>=6.0.2 random_events>=4.1.0 pint>=0.21.1 gymnasium  pin==2.7.0 inflection>=0.5.1 manifold3d==3.0.1 transforms3d python-box urdf_parser_py networkx
-
 EXPOSE 11311
 
 
@@ -31,6 +52,7 @@ RUN apt-get update && apt-get install -y \
     clang \
     cmake \
     make \
+    wget \
     libeigen3-dev \
     libspdlog-dev \
     libraptor2-dev \
@@ -55,6 +77,7 @@ RUN apt-get update && apt-get install -y \
     git \
     ros-noetic-catkin \
     ros-noetic-tf2-geometry-msgs \
+    ros-noetic-xacro \
     swi-prolog*
 
 # KnowRob dependencies
@@ -65,11 +88,29 @@ RUN apt install -y swi-prolog libspdlog-dev \
     doxygen graphviz
 RUN apt install -y ros-noetic-tf2-geometry-msgs
 
+# Download konclude
+WORKDIR $HOME
+RUN wget https://github.com/sasjonge/semantic-map-lab/raw/refs/heads/dev/konclude/Konclude
+# Install konclude
+COPY --chown=${NB_USER}:users konclude ${HOME}/konclude
+ENV  PATH=${PATH}:${HOME}/konclude
+ENV  PYTHONPATH=${PYTHONPATH}:${HOME}/konclude
+
+# Install SOMA DFL reasoner library
+WORKDIR $HOME
+RUN pip install inflection
+RUN git clone https://github.com/ease-crc/ease_lexical_resources.git 
+WORKDIR $HOME/ease_lexical_resources/
+RUN git fetch && \
+git checkout 880248886ed1bccacfbb86a37d2803312aa7122a
+WORKDIR $HOME/ease_lexical_resources/src
+RUN pip install -e .
+
 # Build workspace with knowrob
 WORKDIR $OVERLAY_WS/src
 RUN git clone https://github.com/knowrob/knowrob.git
 RUN git clone https://github.com/knowrob/knowrob_ros.git
-RUN git clone https://github.com/knowrob/knowrob_designator.git
+RUN git clone https://github.com/knowrob/knowrob_designator.git -b add_designator_parsing
 WORKDIR $OVERLAY_WS
 RUN /usr/bin/catkin init
 RUN . /opt/ros/noetic/setup.sh && /usr/bin/catkin build
@@ -78,8 +119,10 @@ RUN . /opt/ros/noetic/setup.sh && /usr/bin/catkin build
 WORKDIR $OVERLAY_WS/src
 ADD . $OVERLAY_WS/src/knowrob_cram_demo
 WORKDIR $OVERLAY_WS
-RUN . /opt/ros/noetic/setup.sh && cd $OVERLAY_WS && catkin build
+RUN . /opt/ros/noetic/setup.sh && catkin build
 RUN echo "source $OVERLAY_WS/devel/setup.bash" >> ~/.bashrc
+# Export Konclude Path
+RUN echo "export KONCLUDE_PATH=${HOME}/konclude" >> ~/.bashrc
 
 COPY run_knowrob_cram_demo.sh /run_knowrob_cram_demo.sh
 
---
+++ reasoner/dfl_reasoner.py
@@ -0,0 +1,197 @@
+from knowrob import *
+
+import dfl.dlquery as dl
+
+def _iriOrVariable(reasoner, x):
+    def _cleanStr(x):
+        for c in ["'", '"']:
+            while c == x[0]:
+                x = x[1:]
+                if c == x[-1]:
+                    x = x[:-1]
+        return str(x)
+    if x.isVariable():
+        return x
+    return IRIAtom(reasoner.expandName(_cleanStr(repr(x))))
+
+def _addToResults(results, variable, bindings):
+    result = bindings.get(variable)
+    if isinstance(result, Blank):
+        return
+    if isinstance(results, set):
+        results.add(str(result))
+    else:
+        results.append(str(result))
+
+class SOMADFLReasoner(GoalDrivenReasoner):
+    def __init__(self):
+        super().__init__()
+        self.reasoner = dl.DFLReasoner()
+        self.reasoner.prefixesDFL["SOMA"] = self.reasoner.prefixesDFL["soma"] 
+        self.hasDisposition = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#hasDisposition")
+        self.isDispositionOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isDispositionOf")
+        self.hasPart = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#hasPart")
+        self.isPartOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isPartOf")
+        self.hasConstituent = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#hasConstituent")
+        self.isConstituentOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isConstituentOf")
+        self.useMatchAtom = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#useMatch")
+        self.useMatch = PredicateIndicator(self.useMatchAtom, 3)
+        self.isInstanceOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isInstanceOf")
+        self.isSubclassOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isSubclassOf")
+        self.rdfType = IRIAtom("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+        self.defineRelation(self.hasDisposition)
+        self.defineRelation(self.hasPart)
+        self.defineRelation(self.hasConstituent)
+        self.defineRelation(self.isDispositionOf)
+        self.defineRelation(self.isPartOf)
+        self.defineRelation(self.isConstituentOf)
+        self.defineRelation(self.useMatch)
+        self.defineRelation(self.isInstanceOf)
+        self.defineRelation(self.isSubclassOf)
+        self.simpleGoals = {self.hasPart, self.hasConstituent, self.hasDisposition, self.isDispositionOf, self.isPartOf, self.isConstituentOf, self.isInstanceOf, self.isSubclassOf}
+        self.inverseProperties = {self.isDispositionOf: self.hasDisposition, self.isPartOf: self.hasPart, self.isConstituentOf: self.hasConstituent}
+        self.fnMap = {self.hasDisposition: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatDispositionsDoesObjectHave, self.reasoner.whatHasDisposition),
+                      self.hasPart: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatPartTypesDoesObjectHave, self.reasoner.whatHasPartType),
+                      self.hasConstituent: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatConstituentsDoesObjectHave, self.reasoner.whatHasConstituent),
+                      self.isSubclassOf: lambda g, p, s, o, bounding: self._evaluateIsSubclassOf(g, p, s, o, bounding),
+                      self.isInstanceOf: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self._ensureIndividual2Classes, self._ensureClass2Individuals)}
+        self.classes = set([self.reasoner.expandName(x) for x in self.reasoner.whatSubclasses('owl:Thing')])
+        
+    def _evaluateIsSubclassOf(self, goal, p, s, o, bounding):
+        if ((False, True) == bounding):
+            v = o
+            retq = self.reasoner.whatSuperclasses(self.reasoner.expandName(str(s)))
+        else:
+            v = s
+            retq = self.reasoner.whatSubclasses(self.reasoner.expandName(str(o)))
+        for sc in retq:
+            bdgs = Bindings()
+            bdgs.set(v, IRIAtom(self.reasoner.expandName(sc)))
+            goal.push(bdgs)
+        return True
+
+    def _evaluateSimpleGoal(self, goal, p, s, o, bounding, fnOVar, fnSVar):
+        if ((False, True) == bounding):
+            todo = self._ensureIndividual2Classes(s)
+            dispositions = set().union(*[fnOVar(str(c)) for c in todo])
+            for d in dispositions:
+                bdgs = Bindings()
+                bdgs.set(o, IRIAtom(self.reasoner.expandName(d)))
+                goal.push(bdgs)
+        else:
+            concepts = [self.reasoner.expandName(c) for c in fnSVar(str(o))]
+            instances = set().union(*[self._ensureClass2Individuals(c) for c in concepts])
+            if ((True, False) == bounding):
+                for i in instances:
+                    bdgs = Bindings()
+                    bdgs.set(s, IRIAtom(self.reasoner.expandName(i)))
+                    goal.push(bdgs)
+            elif ((False, False) == bounding) and str(s) in instances:
+                goal.push(Bindings())
+        return True
+
+    def _ensureIndividual2Classes(self, entity):
+        entity = str(entity)
+        entity = self.reasoner.expandName(entity)
+        retq = [entity]
+        if entity not in self.classes:
+            query_term = GraphSequence([GraphPattern(IRIAtom(entity), self.rdfType, Variable("Z"))])
+            retq = []
+            self.storage().query(GraphQuery(query_term), lambda bindings : _addToResults(retq, "Z", bindings))
+            retq = list(set(retq).union(*[self.reasoner.whatSuperclasses(str(c)) for c in retq]))
+        return retq
+        
+    def _ensureClass2Individuals(self, entity):
+        entity = str(entity)
+        entity = self.reasoner.expandName(entity)
+        retq = [entity]
+        if entity in self.classes:
+            retq = set()
+            for sc in self.reasoner.whatSubclasses(str(entity)):
+                sc = self.reasoner.expandName(sc)
+                query_term = GraphSequence([GraphPattern(Variable("Z"), self.rdfType, IRIAtom(sc))])
+                self.storage().query(GraphQuery(query_term), lambda bindings : _addToResults(retq, "Z", bindings))
+            retq = list(retq)
+        return retq
+                
+    def initializeReasoner(self, config: PropertyTree) -> bool:
+        # TODO: parse and use config.
+        # Flags that would make sense:
+        #     ontology and usematch files to use
+        #     namespace prefixes
+        self.reasoner.initializeOntology()
+        self.classes = set([self.reasoner.expandName(x) for x in self.reasoner.whatSubclasses("owl:Thing")])
+        return True
+
+    def evaluate(self, goal: Goal) -> bool:
+        # Assume only simple goals for now.
+        literal = goal.formula().literals()[0].predicate()
+        p = _iriOrVariable(self.reasoner, literal.functor())
+        if p in self.simpleGoals:
+            s : Term = _iriOrVariable(self.reasoner, literal.arguments()[0])
+            o : Term = _iriOrVariable(self.reasoner, literal.arguments()[1])
+            args = [x for x in [s, o] if not x.isVariable()]
+            if p in self.inverseProperties:
+                p = self.inverseProperties[p]
+                x = s
+                s = o
+                o = x
+            bounding = (s.isVariable(), o.isVariable())
+            if (True, True) == bounding:
+                raise ValueError("Must specify at least one of the participants in the %s goal." % str(p))
+            self.fnMap[p](goal, p, s, o, bounding)
+        else: # useMatch goal
+            task : Term = _iriOrVariable(self.reasoner, literal.arguments()[0])
+            instrument : Term = _iriOrVariable(self.reasoner, literal.arguments()[1])
+            patient : Term = _iriOrVariable(self.reasoner, literal.arguments()[2])
+            # TODO: maybe implement questions of type "what could I do with these objects"
+            if task.isVariable():
+                raise ValueError("Task must be specified for useMatch query")
+            bounding = tuple([x.isVariable() for x in [instrument, patient]])
+            # TODO: maybe implement questions of type "are there any objects around I could use for task?"
+            if (True, True) == bounding:
+                raise ValueError("Must specify at least one of patient or instrument in a useMatch query")
+            if ((False, True) == bounding):
+                todo = self._ensureIndividual2Classes(instrument)
+                classes = set().union(*[self.reasoner.whatObjectsCanToolPerformTaskOn(str(task), c) for c in todo])
+                classes = [self.reasoner.expandName(c) for c in classes]
+                instances = set().union(*[self._ensureClass2Individuals(c) for c in classes])
+                for i in instances:
+                    bdgs = Bindings()
+                    bdgs.set(instrument, IRIAtom(self.reasoner.expandName(i)))
+                    goal.push(bdgs)
+
+            else:
+                todo = self._ensureIndividual2Classes(patient)
+                classes = set().union(*[self.reasoner.whatToolsCanPerformTaskOnObject(str(task), c) for c in todo])
+                classes = [self.reasoner.expandName(c) for c in classes]
+                instances = set().union(*[self._ensureClass2Individuals(c) for c in classes])
+                if (True, False) == bounding:
+                    for i in instances:
+                        bdgs = Bindings()
+                        bdgs.set(instrument, IRIAtom(self.reasoner.expandName(i)))
+                        goal.push(bdgs)
+                elif ((False, False) == bounding) and (str(instrument) in instances):
+                    goal.push(Bindings())
+        return True # False: reasoning error
+
+
+
+
+
+
+
+
+
+'''
+            fn = self.fnMap.get(str(p),{}).get(bounding)
+            if fn is None:
+                raise AttributeError("Query not implemented yet for predicate %s and bounding pattern %s" % (str(p), str(bounding)))
+            fn(*args)
+            #query_term = GraphSequence([
+            #    GraphPattern(s, self.loves, Variable("z")),
+            #    GraphPattern(o, self.loves, Variable("z")),
+            #    GraphBuiltin.notEqual(s, o)])
+            #self.storage().query(GraphQuery(query_term), goal.push)
+        return True
+'''     
