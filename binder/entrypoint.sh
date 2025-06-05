32825a860c28cdc2a00704517cbcfb6e3fc214cd
Sascha Jongebloed <sasjonge@uni-bremen.de>
2025-06-05 11:44:14 +0200

    added example notebook

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Starting entrypoint script..."
 source ${ROS_WS}/devel/setup.bash
-roslaunch --wait knowrob_cram_demo knowrob.launch &
+roslaunch rvizweb rvizweb.launch config_file:=${ROS_WS}/src/knowrob_cram_demo/binder/rviz_configs/pr2_config.json &
 sleep 5
-roslaunch --wait rvizweb rvizweb.launch config_file:=${ROS_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
+roslaunch --wait knowrob_cram_demo knowrob.launch &
 cp ${ROS_WS}/src/knowrob_cram_demo/binder/webapps.json ${ROS_WS}/src/rvizweb/webapps/app.json
 
 xvfb-run exec "$@"
