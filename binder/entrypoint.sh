8700499e5e6e28776d608d4231ed648c4ff05a48
Sascha Jongebloed <sasjonge@uni-bremen.de>
2025-06-17 12:52:33 +0200

    removed webapps

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -7,8 +7,7 @@ roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${ROS_WS}/src/knowrob_cram_demo/binder/rviz_configs/pr2_config.json &
 sleep 5
 roslaunch --wait knowrob_cram_demo knowrob.launch &
-cp ${ROS_WS}/src/knowrob_cram_demo/binder/webapps.json ${ROS_WS}/src/rvizweb/webapps/app.json
 
 jupyter lab workspaces import ${ROS_WS}/src/knowrob_cram_demo/binder/knowrob_cram_demo.jupyterlab-workspace
 
-xvfb-run exec "$@"
+exec "$@"
